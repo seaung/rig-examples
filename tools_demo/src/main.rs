59da7c933f8981ea32b6535d7c1461716cb3ba99
seaung <seaung.c0m7de@protonmail.com>
2025-02-28 19:10:55 +0800

    feat: 添加tools示例

---
+++ tools_demo/src/main.rs
@@ -0,0 +1,108 @@
+use anyhow::Result;
+use rig::{completion::Prompt};
+use rig::completion::request::ToolDefinition;
+use rig::providers::openai;
+use rig::tool::Tool;
+use serde_json::json;
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize)]
+struct OperationArgs {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("Math error")]
+struct MathError;
+
+#[derive(Deserialize, Serialize)]
+struct Adder;
+
+impl Tool for Adder {
+    const NAME: &'static str = "add";
+
+    type Error = MathError;
+    type Args = OperationArgs;
+    type Output = i32;
+
+    async fn definition(&self, _prompt: String) -> ToolDefinition {
+        ToolDefinition {
+            name: "add".to_string(),
+            description: "Add x and y together".to_string(),
+            parameters: json!({
+                "type": "object",
+                "properties": {
+                    "x": {
+                        "type": "number",
+                        "description": "The first number to add"
+                    },
+                    "y": {
+                        "type": "number",
+                        "description": "The second number to add"
+                    }
+                }
+            }),
+        }
+    }
+
+    async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+        println!("[tool-call] Adding {} and {}", args.x, args.y);
+        let result = args.x + args.y;
+        Ok(result)
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Subtract;
+
+impl Tool for Subtract {
+    const NAME: &'static str = "subtract";
+
+    type Error = MathError;
+    type Args = OperationArgs;
+    type Output = i32;
+
+    async fn definition(&self, _prompt: String) -> ToolDefinition {
+        serde_json::from_value(json!({
+            "name": "subtract",
+            "description": "Subtract y from x (i.e.: x - y)",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "x": {
+                        "type": "number",
+                        "description": "The number to subtract from"
+                    },
+                    "y": {
+                        "type": "number",
+                        "description": "The number to subtract"
+                    }
+                }
+            }
+        }))
+            .expect("Tool Definition")
+    }
+
+    async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+        println!("[tool-call] Subtracting {} from {}", args.y, args.x);
+        let result = args.x - args.y;
+        Ok(result)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let client = openai::Client::from_url("ollama", "http://localhost:11434/v1");
+
+    let agent = client.agent("qwen2:7b")
+        .preamble("你是用来帮助用户执行算术运算的计算器。请使用提供的工具来回答用户的问题。")
+        .tool(Adder)
+        .build();
+
+    println!("计算 1 + 1");
+    let r = agent.prompt("计算: 1+1的值").await?;
+    println!("计算结果 : {:#?}", r);
+
+    Ok(())
+}
